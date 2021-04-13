de7837df3034daee2982004af6fe27fcc969d8d9
HakimNurubay <woposo37@gmail.com>
2021-04-13 16:53:15 +0700

    temporary

---
+++ LHT-SANS/assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,16 @@
+<?php
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
